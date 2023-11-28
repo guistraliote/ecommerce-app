d16b4162bdd627267820ca0e05cd586bb34a0a0a
guistraliote <gui.ssh@gmail.com>
2023-11-28 20:40:45 -0300

    first commit

---
+++ src/main/java/com/gstraliote/EcommerceAppApplication.java
@@ -0,0 +1,13 @@
+package com.gstraliote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gstraliote/EcommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gstraliote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
