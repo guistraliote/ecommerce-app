c508d68465cf84245fef60cbc300ec34edd3c8a0
Guilherme Straliote Gonçalves Oliveira <guilherme.straliote@>
2024-01-11 14:42:54 -0300

    H2 database configuration

--- src/main/java/com/gstraliote/EcommerceAppApplication.java
+++ src/main/java/com/gstraliote/EcommerceAppApplication.java
@@ -2,8 +2,9 @@ package com.gstraliote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class EcommerceAppApplication {
 
 	public static void main(String[] args) {
