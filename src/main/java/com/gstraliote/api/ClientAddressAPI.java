1c622633af1e10f41fb09dab1dfef64733ed328a
guistraliote <gui.ssh@gmail.com>
2024-01-14 20:58:23 -0300

    Finishing ClientAddressAPI mapping

---
+++ src/main/java/com/gstraliote/api/ClientAddressAPI.java
@@ -0,0 +1,74 @@
+package com.gstraliote.api;
+
+import com.gstraliote.dto.ClientAddressDTO;
+import com.gstraliote.services.ClientAddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/address")
+public class ClientAddressAPI {
+
+    private final ClientAddressService clientAddressService;
+
+    @Autowired
+    public ClientAddressAPI(ClientAddressService clientAddressService) {
+        this.clientAddressService = clientAddressService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClientAddressDTO>> getAllClientAddressesPaged(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+        Page<ClientAddressDTO> pageResult = clientAddressService.findAllPaged(pageRequest);
+        List<ClientAddressDTO> list = pageResult.getContent();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientAddressDTO> findClientById(@PathVariable Long id) {
+        ClientAddressDTO dto = clientAddressService.findClientAddressById(id);
+
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientAddressDTO> createClientAddress(@RequestBody ClientAddressDTO inputDto) {
+        ClientAddressDTO createdDto = clientAddressService.createClientAddress(inputDto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(createdDto.id()).toUri();
+
+        return ResponseEntity.created(uri).body(createdDto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientAddressDTO> updateClientAddress(
+            @PathVariable Long id,
+            @RequestBody ClientAddressDTO inputDto) {
+        ClientAddressDTO updatedDto = clientAddressService.updateClientAddress(id, inputDto);
+
+        return ResponseEntity.ok().body(updatedDto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteClientAddressById(@PathVariable Long id) {
+        clientAddressService.deleteClientAddressById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
