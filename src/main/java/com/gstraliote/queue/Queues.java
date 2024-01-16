f79d2095b76b12b393fbb862317b6da792238c99
Guilherme Straliote Gonçalves Oliveira <guilherme.straliote@>
2024-01-16 16:56:37 -0300

    ActiveMQ first configuration

---
+++ src/main/java/com/gstraliote/queue/Queues.java
@@ -0,0 +1,16 @@
+package com.gstraliote.queue;
+
+public final class Queues {
+
+    public static final String CATEGORY = "Category";
+    public static final String CLIENT = "Client";
+    public static final String CLIENT_ADDRESS = "ClientAddress";
+    public static final String ORDER = "Order";
+    public static final String PAYMENT_METHOD = "PaymentMethod";
+    public static final String PRODUCT = "Product";
+    public static final String PRODUCT_REVIEW = "ProductReview";
+    public static final String USER_CREDENTIALS = "UserCredentials";
+
+    private Queues() {
+    }
+}
